a3fbf69d9b790d4f74b44c9c2a8d7cee3064ed26
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 17:40:59 +0100

    fixing function to account for case sensitivity

--- src/utils/filterResources.ts
+++ src/utils/filterResources.ts
@@ -1,6 +1,6 @@
 import { Resource } from "../types/resource";
 export const filterResources = (resources: Resource[], searchTerm: string) => {
     return resources.filter(resource => {
-        return resource.title.includes(searchTerm);
+        return resource.title.toLowerCase().includes(searchTerm.toLowerCase());
     })
 }
