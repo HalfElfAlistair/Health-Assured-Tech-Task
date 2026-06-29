6fa397f3a438eda8e77f70bc6ffdbc7047e5e81f
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 09:37:19 +0100

    adding Category enum and Resource type definitions

---
+++ src/types/resource.ts
@@ -0,0 +1,20 @@
+// Category Enum to ensure consistent values when I add grouping, filtering, and UI components
+export enum Category {
+    Podcasts = "Podcasts",
+    Articles = "Articles",
+    Newsletters = "Newsletters",
+    Recipes = "Recipes",
+    Fitness = "Fitness",
+    Meditation = "Meditation"
+};
+
+export interface Resource {
+    id: string;
+    category: string;
+    title: string;
+    thumbnail: string;
+    tags: string[],
+    duration: Number,
+    description: string;
+    date_uploaded: string;
+}
