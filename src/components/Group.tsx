fbcf76850a2affb1ce414cbc091dc350579dee01
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 22:25:20 +0100

    adding a styling span to mimic design style from Health Assured website

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -10,6 +10,7 @@ export const Group = ({ category, resources }: GroupProps) => {
     return (
         <section>
             <h2>{category}</h2>
+            <span />
             {resources.map(resource => {
                 return (
                     <ResourceCard key={resource.id} resource={resource} />
