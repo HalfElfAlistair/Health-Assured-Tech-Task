bf8332f1cf75458dc69d6bdf54fba2103295602d
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 12:58:57 +0100

    importing resources JSON to app and rendering each category title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import rawResources from './data/resources.json';
+import { groupResources } from './utils/groupResources';
+import { Resource } from './types/resource';
 
 function App() {
-  return (
-    <>
-      App
-    </>
-  )
+  const resources = rawResources as Resource[];
+  const groupedResources = groupResources(resources);
+  return Object.keys(groupedResources).map(category => {
+    return <h1>{category}</h1>
+  })
 }
 
 export default App
