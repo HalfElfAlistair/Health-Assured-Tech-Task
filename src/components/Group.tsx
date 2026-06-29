fd0807742ba07461bc72621e7df26dc670f72b72
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 17:59:01 +0100

    removing sort and filtering functionality from Group component

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,19 +1,16 @@
 import { Category, Resource } from "../types/resource";
 import { ResourceCard } from "./ResourceCard";
-import { sortResources } from "../utils/sortResources";
 
 interface GroupProps {
     category: Category;
     resources: Resource[];
-    sortBy: string;
 }
 
-export const Group = ({ category, resources, sortBy }: GroupProps) => {
-    const sortedResources = sortResources(resources, sortBy);
+export const Group = ({ category, resources }: GroupProps) => {
     return (
         <section>
             <h2>{category}</h2>
-            {sortedResources.map(resource => {
+            {resources.map(resource => {
                 return (
                     <ResourceCard key={resource.id} resource={resource} />
                 )
