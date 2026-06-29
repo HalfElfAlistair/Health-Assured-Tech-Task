447d698687ef6be30a4fe15266d64fc299f20f0e
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 17:31:49 +0100

    adding filter functionality to filterResources function

--- src/utils/filterResources.ts
+++ src/utils/filterResources.ts
@@ -1,4 +1,6 @@
 import { Resource } from "../types/resource";
-export const filterResources = (resources: Resource[]) => {
-    return resources;
+export const filterResources = (resources: Resource[], searchTerm: string) => {
+    return resources.filter(resource => {
+        return resource.title.includes(searchTerm);
+    })
 }
