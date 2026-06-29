8ff807f9fd0db00456a958a8511605aa9bbba20b
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 15:39:52 +0100

    returning sort callback to order by newest first

--- src/utils/sortResources.ts
+++ src/utils/sortResources.ts
@@ -1,4 +1,6 @@
 import { Resource } from "../types/resource"
 export const sortResources = (resources: Resource[]) => {
-    return resources;
+    return resources.sort((a: Resource, b: Resource) => {
+        return new Date(b.date_uploaded).getTime() - new Date(a.date_uploaded).getTime();
+    })
 }
