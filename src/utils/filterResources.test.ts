6be8be525010a9421c26dfd69f0f054a35766fa8
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 17:04:52 +0100

    adding basic failing test for filterResources

---
+++ src/utils/filterResources.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, test } from 'vitest';
+import { filterResources } from './filterResources';
+import { Category, Resource } from '../types/resource';
+
+describe("filterResources", () => {
+
+    const resources: Resource[] = [
+        {
+            "id": "001",
+            "category": Category.Podcasts,
+            "title": "Podcast A",
+            "thumbnail": "https://www.datocms-assets.com/93767/1753971746-photo.jpg",
+            "tags": ["wellbeing", "mindfulness", "relaxation"],
+            "duration": 25,
+            "description": "A calming podcast focused on mindfulness techniques for daily life.",
+            "date_uploaded": "2025-07-10"
+        },
+        {
+            "id": "002",
+            "category": Category.Podcasts,
+            "title": "Article A",
+            "thumbnail": "https://www.datocms-assets.com/93767/1751891809-photo.jpg",
+            "tags": ["wellbeing", "sleep", "science"],
+            "duration": 8,
+            "description": "Explore the latest research on how sleep affects mental and physical health.",
+            "date_uploaded": "2025-06-22"
+        },
+        {
+            "id": "003",
+            "category": Category.Podcasts,
+            "title": "Podcast B",
+            "thumbnail": "https://www.datocms-assets.com/93767/1753971221-photo.jpg",
+            "tags": ["community", "tips", "mindfulness"],
+            "duration": 5,
+            "description": "Your weekly dose of wellness tips and updates from our experts.",
+            "date_uploaded": "2025-08-01"
+        },
+    ];
+
+    test("returns empty array if passed an empty array", () => {
+        const filteredResources = filterResources([]);
+        expect(filteredResources).toEqual([]);
+    })
+})
