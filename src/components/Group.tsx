f7b1a7d8543aa02f673da474e1afa79934435d5d
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 14:06:42 +0100

    adding loop through resources for each group, that renders resources with ResourceCard component

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,4 +1,5 @@
 import { Category, Resource } from "../types/resource";
+import { ResourceCard } from "./ResourceCard";
 
 interface GroupProps {
     category: Category;
@@ -10,6 +11,11 @@ export const Group = ({ category, resources, sortBy }: GroupProps) => {
     return (
         <section>
             <h2>{category}</h2>
+            {resources.map(resource => {
+                return (
+                    <ResourceCard key={resource.id} resource={resource} />
+                )
+            })}
         </section>
     )
 }
