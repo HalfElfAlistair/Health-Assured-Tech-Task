a3fc9b556347c9df37b0985a88fa51ec57117979
HalfElfAlistair <alistairjss@gmail.com>
2026-06-29 10:05:41 +0100

    implementing basic grouping logic to satisfy test

--- src/utils/groupResources.ts
+++ src/utils/groupResources.ts
@@ -1,1 +1,17 @@
-export const groupResources = () => { }
+import { Resource } from '../types/resource';
+
+interface ResourceCategories {
+    [key: string]: Resource[];
+}
+
+export const groupResources = (resources: Resource[]) => {
+    const result: ResourceCategories = {};
+    resources.forEach(resource => {
+        const { category } = resource;
+        if (!result[category]) {
+            result[category] = [];
+        }
+        result[category].push(resource);
+    })
+    return result;
+}
