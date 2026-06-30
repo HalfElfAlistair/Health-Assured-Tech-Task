174b5538f050dd9b156bf04e967dbafd778fc476
HalfElfAlistair <alistairjss@gmail.com>
2026-06-30 08:51:09 +0100

    cloning resources before sort mutation, to ensure function works with memoization

--- src/utils/sortResources.ts
+++ src/utils/sortResources.ts
@@ -1,6 +1,8 @@
 import { Resource } from "../types/resource"
 export const sortResources = (resources: Resource[], sortBy: string) => {
-    return resources.sort((a: Resource, b: Resource) => {
+    // clones resources to prevent mutation of the argument
+    const clonedResources = [...resources];
+    return clonedResources.sort((a: Resource, b: Resource) => {
         return sortBy === "New" ? (
             new Date(b.date_uploaded).getTime() - new Date(a.date_uploaded).getTime()
         ) : (
