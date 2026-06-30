43d64118672643188102da98f14f2378cc5dde23
HalfElfAlistair <alistairjss@gmail.com>
2026-06-30 16:47:23 +0100

    adding a helper for date sort testing

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
 test('homepage loads', async ({ page }) => {
     await page.goto('http://localhost:5173');
     await expect(page.getByRole('heading', { name: 'Podcasts' })).toBeVisible();
 });
+
+// extracts dates from cards and convert to timestamps
+const getCardDates = async (page: Page): Promise<number[]> => {
+    const dateStrings: string[] = await page
+        .locator('[data-testid="resource-card"] [data-testid="resource-date"]')
+        .allTextContents();
+    return dateStrings.map((str: string) => new Date(str).getTime());
+}
