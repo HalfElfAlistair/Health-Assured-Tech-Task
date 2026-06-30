1fab90e53591cbe542ca5513a859d277b2883ad7
HalfElfAlistair <alistairjss@gmail.com>
2026-06-30 10:56:07 +0100

    adding a note and moving mockResources within test

--- src/components/Group/Group.test.tsx
+++ src/components/Group/Group.test.tsx
@@ -5,24 +5,27 @@ import { Category, Resource } from '../../types/resource';
 
 describe('Group', () => {
     const mockCategory = Category.Podcasts;
-    const mockResources: Resource[] = [
-        {
-            "id": "001",
-            "category": Category.Podcasts,
-            "title": "Mindful Moments",
-            "thumbnail": "https://www.datocms-assets.com/93767/1753971746-photo.jpg",
-            "tags": [
-                "wellbeing",
-                "mindfulness",
-                "relaxation"
-            ],
-            "duration": 25,
-            "description": "A calming podcast focused on mindfulness techniques for daily life.",
-            "date_uploaded": "2025-07-10"
-        }
-    ]
+
     test('renders the category heading', () => {
+        const mockResources: Resource[] = [
+            {
+                "id": "001",
+                "category": Category.Podcasts,
+                "title": "Mindful Moments",
+                "thumbnail": "https://www.datocms-assets.com/93767/1753971746-photo.jpg",
+                "tags": [
+                    "wellbeing",
+                    "mindfulness",
+                    "relaxation"
+                ],
+                "duration": 25,
+                "description": "A calming podcast focused on mindfulness techniques for daily life.",
+                "date_uploaded": "2025-07-10"
+            }
+        ]
+
         render(<Group category={mockCategory} resources={mockResources} />);
+        // checks for a match in rendered heading text
         expect(screen.getByRole('heading', { name: /podcast/i })).toBeInTheDocument();
     })
 })
